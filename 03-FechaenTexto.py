cb973683504ddae22514e15b6875f48308f72708
chantal-PF <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2024-08-05 23:29:08 -0500

    Add files via upload

---
+++ 03-FechaenTexto.py
@@ -0,0 +1,43 @@
+dia = int(input("Ingrese tu dia: "))
+mes = input("Ingrese tu mes: ")
+año = input("ingrese tu año: ")
+
+match mes:
+    case "enero":
+        enero = 1
+        print(dia , enero , año)
+    case "febrero":
+        febrero = 2
+        print(dia , febrero , año)
+    case "marzo":
+        marzo = 3
+        print(dia , marzo , año)
+    case "abril":
+        abril = 4
+        print(dia , abril , año)
+    case "mayo":
+        mayo = 5
+        print(dia , mayo , año)
+    case "junio":
+        junio = 6
+        print(dia , junio , año)
+    case "julio":
+        julio = 7
+        print(dia , julio , año)
+    case "agosto":
+        agosto = 8
+        print(dia , agosto , año)
+    case "septiembre":
+        septiembre = 9
+        print(dia , septiembre , año)
+    case "octubre":
+        octubre = 10
+        print(dia , octubre , año)
+    case "noviembre":
+        noviembre = 11
+        print(dia , noviembre , año)
+    case "diciembre":
+        diciembre = 12
+        print(dia , diciembre , año)
+
+ 
