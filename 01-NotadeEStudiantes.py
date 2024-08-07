4a0035b35f992f4913669c88e08e4e714c1600e7
chantal-PF <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2024-08-06 20:00:10 -0500

    Update 01-NotadeEStudiantes.py

--- 01-NotadeEStudiantes.py
+++ 01-NotadeEStudiantes.py
@@ -5,13 +5,13 @@ examen = int(input("Examen: "))
 suma = nota1 + nota2 + nota3 + examen
 promedio = suma / 4
 
-if promedio >= 90:
+if promedio > 89 and promedio < 101:
     print("A")
-elif promedio >= 80:
+elif promedio > 79 and promedio < 90:
     print("B")
-elif promedio >= 70:
+elif promedio > 69 and promedio < 80:
     print("C")
-elif promedio >= 60:
+elif promedio > 59 and promedio < 70:
     print("D")
 else:
     print("E")
