0fc827b20d2ad6aa1635226dfa0abfc0259de0dc
chantal-PF <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2024-08-06 19:21:46 -0500

    Update 03-FechaenTexto.py

--- 03-FechaenTexto.py
+++ 03-FechaenTexto.py
@@ -5,39 +5,40 @@ año = input("ingrese tu año: ")
 match mes:
     case "enero":
         enero = 1
-        print(dia , enero , año)
+        print(dia ,"/", enero ,"/", año)
     case "febrero":
         febrero = 2
-        print(dia , febrero , año)
+        print(dia ,"/", febrero ,"/", año)
     case "marzo":
         marzo = 3
-        print(dia , marzo , año)
+        print(dia ,"/", marzo ,"/", año)
     case "abril":
         abril = 4
-        print(dia , abril , año)
+        print(dia ,"/", abril ,"/", año)
     case "mayo":
         mayo = 5
-        print(dia , mayo , año)
+        print(dia ,"/", mayo ,"/", año)
     case "junio":
         junio = 6
-        print(dia , junio , año)
+        print(dia ,"/", junio ,"/", año)
     case "julio":
         julio = 7
-        print(dia , julio , año)
+        print(dia ,"/", julio ,"/", año)
     case "agosto":
         agosto = 8
-        print(dia , agosto , año)
+        print(dia ,"/", agosto ,"/", año)
     case "septiembre":
         septiembre = 9
-        print(dia , septiembre , año)
+        print(dia ,"/", septiembre ,"/", año)
     case "octubre":
         octubre = 10
-        print(dia , octubre , año)
+        print(dia ,"/", octubre ,"/", año)
     case "noviembre":
         noviembre = 11
-        print(dia , noviembre , año)
+        print(dia ,"/", noviembre ,"/", año)
     case "diciembre":
         diciembre = 12
-        print(dia , diciembre , año)
+        print(dia , "/" , diciembre , "/" , año)
+
 
  
